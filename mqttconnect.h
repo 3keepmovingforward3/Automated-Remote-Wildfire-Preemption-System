37543a5cf02973da9d85b31ed1d6274f776b1410
Benjamin Blouin <3keepmovingforward3@users.noreply.github.com>
2019-05-05 10:57:55 -0400

    Add files via upload

---
+++ mqttconnect.h
@@ -0,0 +1,25 @@
+#ifndef MQTTCONNECT_H
+#define MQTTCONNECT_H
+
+void mqttconnect(PubSubClient client) {
+  /* Loop until reconnected */
+  while (!client.connected()) {
+    Serial.print("MQTT connecting ...");
+    /* client ID */
+    String clientId = "ESP32Client";
+    /* connect now */
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      /* subscribe topic with default QoS 0*/
+      client.subscribe(LED_TOPIC);
+    } else {
+      Serial.print("failed, status code =");
+      Serial.print(client.state());
+      Serial.println("try again in 5 seconds");
+      /* Wait 5 seconds before retrying */
+      delay(5000);
+    }
+  }
+}
+
+#endif
